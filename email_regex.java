ff02753d542ae081ec37684d56cde3a93d8d88a5
sachin srivastava <90714874+sachin053@users.noreply.github.com>
2021-12-05 23:00:30 +0530

    Add files via upload

---
+++ email_regex.java
@@ -0,0 +1,36 @@
+// Java program to check if an email address
+// is valid using Regex.
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.*;
+
+class Test
+{
+    public static boolean isValid(String email)
+    {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+
+        Pattern pat = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return pat.matcher(email).matches();
+    }
+
+    public static void main(String[] args)
+    {
+        ArrayList<String> address = new ArrayList<>();
+
+        address.add("sachinkj@gmail.com");
+        address.add("sachinkjgmail.com");
+
+        for(String i : address){
+            if (isValid(i))
+                System.out.println(i + " - Yes");
+            else
+                System.out.println(i + " - No");
+        }
+    }
+}
