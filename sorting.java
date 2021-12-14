f5b626ab7edd512c70e4fe98cd79d34be7b2da45
sachin srivastava <90714874+sachin053@users.noreply.github.com>
2021-12-14 23:52:16 +0530

    Add files via upload

---
+++ sorting.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+// representation of comparator interface for customize sorting 
+
+class Emp20
+{
+  public String name;
+  public int  id;
+  Emp20(String name, int id)
+  {
+    this.name = name;
+    this.id = id;
+  }
+  @Override
+  public String toString() {
+    return name  +
+            " "+id;
+  }
+}
+
+// here sort  the data by using the name 
+
+class Sortbyname implements Comparator<Emp20>
+{
+  @Override
+  public int compare(Emp20 o1, Emp20 o2) {
+    return o1.name.compareTo(o2.name);
+  }
+}
+
+// here sort the data by using the  id 
+
+class Sortbyid implements Comparator<Emp20>
+{
+  @Override
+  public int compare(Emp20 o1, Emp20 o2) {
+    if(o1.id> o2.id)
+    {
+      return 1;
+    }
+    if (o1.id< o2.id)
+    {
+      return -1;
+    }
+    return 0;
+  }
+}
+public class ComExample1 {
+  public static void main(String[] args) {
+    ArrayList<Emp20> obj=  new ArrayList<>();
+    obj.add(new Emp20("sachin", 102));
+    obj.add(new Emp20("rahul", 106));
+    obj.add(new Emp20("kunal", 104));
+    obj.add(new Emp20("ranjeet", 103)); 
+    Collections.sort(obj,new Sortbyname());
+    System.out.println(obj);
+
+  }
+}
